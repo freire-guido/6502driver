d6f3cbb078dfc51c3012c1a44e75212961365727
freire-guido <freireguido@outlook.com>
2022-03-03 17:04:14 -0300

    built-in EEPROM

--- dataprint/dataprint.ino
+++ dataprint/dataprint.ino
@@ -1,9 +1,7 @@
+#include <EEPROM.h>
 #include "busmanager.h"
 
 const int addr_bus[8] = {4, 5, A0, A1, A2, A3, A4, A5};
-const int data_bus[8] = {6, 7, 8, 9, 10, 11, 12, 13};
-
-byte ROM[256];
 
 void setup() {
   Serial.begin(9600);
@@ -14,7 +12,7 @@ void setup() {
 
 void loop() {
   for (int i = 0; i < 256; i++) {
-    Serial.print(ROM[i]);
+    Serial.print(EEPROM[i]);
     Serial.print(" ");
     if (i % 16 == 15) {
       Serial.println("");
