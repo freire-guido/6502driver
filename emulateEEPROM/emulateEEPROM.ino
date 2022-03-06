0086300b23b7e75a0fc27b7436d2fe708859959c
freire-guido <freireguido@outlook.com>
2022-03-05 22:45:13 -0300

    serial into eeprom bytes

--- emulateEEPROM/emulateEEPROM.ino
+++ emulateEEPROM/emulateEEPROM.ino
@@ -3,7 +3,6 @@
 
 const int addr_bus[8] = {4, 5, A0, A1, A2, A3, A4, A5};
 const int data_bus[8] = {6, 7, 8, 9, 10, 11, 12, 13};
-int Ei = 0;
 
 void setup() {
   for (int addr : addr_bus) {
@@ -14,21 +13,10 @@ void setup() {
   }
   eepromClear();
   Serial.begin(9600);
-//  hexdump();
 }
 
 void loop() {
   
-}
-
-void serialEvent() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  String buf;
-  buf = Serial.readString();
-  for (int i = 0; i < buf.length(); i++) {
-    Serial.write(buf[i]);
-  }
-  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void eepromClear() {
@@ -48,8 +36,32 @@ void hexdump() {
   Serial.print('\n');
 }
 
-byte strToBytes(String str, byte bytes[], int _sz = -1) {
-  for (int i = 0; i < _sz || (_sz == -1 && str[i] == '\n'); i) {
-    
+void strToBytes(String str, byte buff[], int _sz, char delim = ' ') {
+  int b = 0;
+  int j = 0;
+  for (int i = 0; b < _sz && j != -1; i = j + 1) {
+    j = str.indexOf(delim, i + 1);
+    buff[b] = byte(str.substring(i, j).toInt());
+    b++;
   }
+}
+
+void serialEvent() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  String message = Serial.readString();
+  Serial.println(message.length());
+  byte buffer[16];
+  int b = 0;
+  int j = 0;
+  for (int i = 0; j < message.length() && j != -1; i = j + 1) {
+    j = message.indexOf('\n', i + 1);
+    strToBytes(message.substring(i, j), buffer, sizeof(buffer));
+    for (int a = 0; a < sizeof(buffer); a++) {
+      EEPROM[b + a] = buffer[a];
+    }
+    b += sizeof(buffer);
+  }
+  digitalWrite(LED_BUILTIN, LOW);
+  hexdump();
+  eepromClear();
 }
--- filetoserial.sh
+++ filetoserial.sh
@@ -1,4 +1,4 @@
 # Communicate binary file line by line to the specified port.
 dasm $1.asm -R -f3 -o$1.bin
 stty -F /dev/ttyACM0 cs8 9600 ignbrk -brkint -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
-od -An -t u1 $1.bin > /dev/ttyACM0
+hexdump -e '16/1 "%u " "\n"' $1.bin > /dev/ttyACM0
