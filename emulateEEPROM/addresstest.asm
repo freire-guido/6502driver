3dde811c3f44b6c8d909b6af024ea897c8d7bf67
freire-guido <freireguido@outlook.com>
2022-03-10 19:32:53 -0300

    unify busses, reset interaction

--- emulateEEPROM/addresstest.asm
+++ emulateEEPROM/addresstest.asm
@@ -1,14 +1,13 @@
     PROCESSOR 6502
 
     ORG $F00
-    LDX #$01
-    LDA #$05
-    STA $01
-    LDA #$07
-    STA $02
-    LDY #$0a
-    STY $0705
-    LDA ($00,X)
+    LDX #$00
+    LDY #$00
+loop:
+    STX $00,Y
+    INY
+    INX
+    JMP loop
 
 ; 0600: a2 01 a9 05 85 01 a9 07 85 02 a0 0a 8c 05 07 a1 
 ; 0610: 00 
--- emulateEEPROM/emulateEEPROM.ino
+++ emulateEEPROM/emulateEEPROM.ino
@@ -1,11 +1,13 @@
 #include <EEPROM.h>
 #include "busmanager.h"
+#include "oscillator.h"
 
-const int addr_bus[16] = {4, 4, 4, 4, 4, 4, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-const int offset = 0xFC00;
-// data bus is actually 10 bits (pins 3 and 5 - 13) long, 4s are trash
-const int data_bus[8] = {1, 2, A0, A1, A2, A3, A4, A5};
+const int addr_bus[16] = {1, 1, 1, 1, 1, 1, 1, 1, 4, 5, A5, A4, A3, A2, A1, A0};
+const int offset = 0xFF00;
+// data bus is actually 8 bits (pins 4 - 5 and A5 - A0) long, 1s are trash
+const int data_bus[8] = {6, 7, 8, 9, 10, 11, 12, 13};
 const int CS = 0;
+const int RS = 1;
 bool serviced = false;
 
 void setup() {
@@ -15,27 +17,32 @@ void setup() {
   for (int data : data_bus) {
     pinMode(data, OUTPUT);
   }
-  pinMode(4, INPUT_PULLUP); // 4s are trash
+  pinMode(1, INPUT_PULLUP); // 1s are trash
   pinMode(CS, INPUT);
 
   eepromClear();
   EEPROM[0xFFFC - offset] = 0x00;
-  EEPROM[0xFFFD - offset] = 0xFC;
+  EEPROM[0xFFFD - offset] = 0xFF;
 
   Serial.begin(115200);
   Serial.println(EEPROM.length());
+  hexdump();
   Serial.println("RDY");
 }
 
 void loop() {
   while (!serviced) {
-    if (digitalRead(CS) == HIGH) {
-      writeBus(data_bus, EEPROM[readBus(addr_bus, 16) - offset]);
+    if (digitalRead(CS) == LOW) {
+      int address = readBus(addr_bus, 16) - offset;
+      writeBus(data_bus, EEPROM[address]);
+      Serial.print(address, BIN);
+      Serial.print(' ');
+      Serial.println(address, HEX);
+
       serviced = true;
       digitalWrite(LED_BUILTIN, HIGH);
     }
-  }
-  if (digitalRead(CS) == LOW) {
+  }  if (digitalRead(CS) == HIGH) {
     serviced = false;
     digitalWrite(LED_BUILTIN, LOW);
   }
@@ -69,20 +76,22 @@ void strToBytes(String str, byte buff[], int _sz, char delim = ' ') {
   }
 }
 
-void serialEvent() {
-  String message = Serial.readString();
-  Serial.println(message.length());
-  byte buffer[16];
-  int b = 0;
-  int j = 0;
-  for (int i = 0; j < message.length() && j != -1; i = j + 1) {
-    j = message.indexOf('\n', i + 1);
-    strToBytes(message.substring(i, j), buffer, sizeof(buffer));
-    for (int a = 0; a < sizeof(buffer); a++) {
-      EEPROM[b + a] = buffer[a];
+void serialEvent() { 
+  if (digitalRead(RS) == LOW) {
+    String message = Serial.readString();
+    Serial.println(message.length());
+    byte buffer[16];
+    int b = 0;
+    int j = 0;
+    for (int i = 0; j < message.length() && j != -1; i = j + 1) {
+      j = message.indexOf('\n', i + 1);
+      strToBytes(message.substring(i, j), buffer, sizeof(buffer));
+      for (int a = 0; a < sizeof(buffer); a++) {
+        EEPROM[b + a] = buffer[a];
+      }
+      b += sizeof(buffer);
     }
-    b += sizeof(buffer);
+    hexdump();
+    eepromClear();
   }
-  hexdump();
-  eepromClear();
 }
